980ea50b837a56319ab87b144c0f90fe1efc47de
pedro <pedro@LAPTOP-505DTEDB>
2020-10-29 11:11:41 +0100

    pruebapp

--- src/main/java/Spotify2/Spotify/App.java
+++ src/main/java/Spotify2/Spotify/App.java
@@ -4,10 +4,10 @@ package Spotify2.Spotify;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "yep cock" );
     }
 }

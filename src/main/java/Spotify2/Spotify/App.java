543c30a30f1930dcadf2b0eeebcc888caccf8085
Manuel <antoniomanu533@gmail.com>
2020-10-30 11:09:37 +0100

    XMLManager hecho

--- src/main/java/Spotify2/Spotify/App.java
+++ src/main/java/Spotify2/Spotify/App.java
@@ -1,5 +1,8 @@
 package Spotify2.Spotify;
 
+import models.Channel;
+import utilities.XMLManager;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Channel c=new Channel();
+        System.out.println( XMLManager.SetChanel(c));
     }
 }
---
+++ src/main/java/utilities/XMLManager.java
@@ -0,0 +1,61 @@
+package utilities;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import models.Channel;
+
+public class XMLManager {
+
+	private static String FILE="Chanel.xml";
+
+	public static boolean SetChanel(Channel c){
+		boolean valid=false;
+		JAXBContext context;
+		FileWriter fw;
+		BufferedWriter bw;
+		Marshaller ms;
+		try {
+			fw=new FileWriter(FILE);
+			bw=new BufferedWriter(fw);
+			try {
+				context=JAXBContext.newInstance(Channel.class);
+				ms=context.createMarshaller();
+				ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+				ms.marshal(c, bw);
+				bw.close();
+				fw.close();
+				valid=true;
+			} catch (JAXBException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return valid;
+	}
+
+	public static Channel GetChannel(){
+		Channel c=null;
+		JAXBContext context;
+		Unmarshaller us;
+		try {
+			context=JAXBContext.newInstance(Channel.class);
+			us=context.createUnmarshaller();
+			c=(Channel) us.unmarshal(new File(FILE));
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			c=new Channel();
+		}
+		return c;
+	}
+
+}
